7bfd26b4766ab62aa84e2266ed3a43a3741b6960
NikhilMarewad <nikhilmarewad@gmail.com>
2019-12-06 09:37:10 +0530

    web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,41 @@
 // Include truYum form validation functions here
+function validateForm() {
+	 var x = document.forms["myForm"]["name1"].value;
+	 var y = document.forms["myForm"]["number1"].value;
+	 var z = document.forms["myForm"]["dates"].value;
+		 
+		  if (x == "") {
+			  alert("Name must be filled out");
+			 
+		  } 
+		  else if (y == "") {
+			  alert("Number must be filled out");
+			 
+		  } 
+		  
+		  else if (z == "") {
+			  alert("Date must be filled out");
+			  return false;
+		  } else {
+			  return true;
+		  }
+		  
+		  if (x.length>2 || x.length<60){
+			 alert("Text is not upto the length 2to60 Characters");
+			 return false;
+		  } else {
+			  return true;
+		  }
+		  if (/[^0-9/]/.test(y)){
+			   alert("number is must");
+		       return false;
+		  } else {
+			  return true;
+		  }
+		   if (/[^A-Za-z]/.test(x)){
+			   alert("letter is must");
+		       return false;
+		  } else {
+			  return true;
+		  }
+}
